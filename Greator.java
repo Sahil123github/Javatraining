ce59ff954ee0e6dd9932e7d3c15cc7268b2b83dd
Sahil123github <104566182+Sahil123github@users.noreply.github.com>
2022-05-05 17:27:18 +0530

    Add files via upload

---
+++ Greator.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Greator {
+    public static void main(String[] args) {
+        int a=0;
+        int b=10;
+
+       if(a>b)
+        {
+            System.out.println("a number is gretor");
+        }
+        else{
+            System.out.println("b number is gretor");
+        }
+
+
+    }
+}
